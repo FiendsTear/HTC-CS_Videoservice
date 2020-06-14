5b3f9ab3aa4f138bb15e3ceb3bef5cda4c79b183
FiendsTear <94nerevar@gmail.com>
2020-06-14 12:38:30 +0300

    fix more linted problems

--- src/components/Movie/Poster/index.js
+++ src/components/Movie/Poster/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { cn } from "@bem-react/classname";
-import "./Poster.css";
-import Curtain from "../../Curtain";
+import { cn } from '@bem-react/classname';
+import './Poster.css';
+import Curtain from '../../Curtain';
+import PropTypes from 'prop-types';
 
 function Poster(props) {
-  const cnPoster = cn("poster");
-  const cnPosterDescription = cn(cnPoster(), "description");
-  const cnPosterPicture = cn(cnPoster(), "picture");
+	const cnPoster = cn('poster');
+	const cnPosterDescription = cn(cnPoster(), 'description');
+	const cnPosterPicture = cn(cnPoster(), 'picture');
 
-  return (
-    <figure className={cnPoster()}>
-      <Curtain covering='poster'></Curtain>
-      <img className={cnPosterPicture()} src={"/assets/posters/" + props.posterName} alt="new_batman"/>
-      <figcaption className={cnPosterDescription()}>{props.posterDescription}</figcaption>
-    </figure>
-  );
+	return (
+		<figure className={cnPoster()}>
+			<Curtain covering='poster'></Curtain>
+			<img className={cnPosterPicture()} src={'/assets/posters/' + props.posterName} alt="new_batman"/>
+			<figcaption className={cnPosterDescription()}>{props.posterDescription}</figcaption>
+		</figure>
+	);
 }
+
+Poster.propTypes = {
+	posterName: PropTypes.string,
+	posterDescription: PropTypes.string
+};
 
 export default Poster;
