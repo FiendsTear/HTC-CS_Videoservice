2773ec75f946130d3b9f3862ac7f869cf3219ff8
FiendsTear <94nerevar@gmail.com>
2020-06-14 12:41:29 +0300

    fix more linted problems 2

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import Poster from "./Poster";
-import { cn } from "@bem-react/classname";
-import "./Movie.css";
+import Poster from './Poster';
+import { cn } from '@bem-react/classname';
+import './Movie.css';
+import PropTypes from 'prop-types';
 
 function Movie(props) {
-  const cnMovie = cn("movie");
+	const cnMovie = cn('movie');
 
-  return (
-    <article className={cnMovie()}>
-      <Poster posterName={props.posterName} posterDescription={props.description}></Poster>
-      <span>{props.name}</span>
-    </article>
-  );
+	return (
+		<article className={cnMovie()}>
+			<Poster posterName={props.posterName} posterDescription={props.description}></Poster>
+			<h6>{props.name}</h6>
+		</article>
+	);
 }
+
+Movie.propTypes = {
+	posterName: PropTypes.string,
+	name: PropTypes.string,
+	description: PropTypes.string
+};
 
 export default Movie;
